5dbae8d8f2247a62859645455ac350eec2a57a6c
ErwanM <erwan.moreac@gmail.com>
2018-11-13 17:53:16 +0100

    Start implement MemoryCoordinator class

--- shared_memory_lib/src/CommSharedMemory.hpp
+++ shared_memory_lib/src/CommSharedMemory.hpp
@@ -1,6 +1,42 @@
 #ifndef COMM_SHARED_MEMORY_H
 #define COMM_SHARED_MEMORY_H
 
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+
+//Alias an STL compatible allocator of ints that allocates ints from the managed
+//shared memory segment.  This allocator will allow to place containers
+//in managed shared memory segments
+typedef boost::interprocess::allocator<int, boost::interprocess::managed_shared_memory::segment_manager> ShmemAllocator;
+//Alias a vector that uses the previous STL-like allocator
+typedef std::vector<int, ShmemAllocator> MyVector;
+
+
+class MemoryCoordinator {
+    private:
+
+    //Construct a shared memory
+    boost::interprocess::offset_ptr<MyVector> C3_table_ptr;
+    boost::interprocess::offset_ptr<MyVector> achievable_ptr;
+    boost::interprocess::offset_ptr<MyVector> release_hw_ptr;
+    boost::interprocess::offset_ptr<MyVector> done_ptr;
+
+    boost::interprocess::offset_ptr<int> ptr;
+
+
+
+
+    public:
+
+};
+
+
+//EM, TODO: DELETE IT
 class Box {
     private:
         double length;   // Length of a box
@@ -16,5 +52,6 @@ class Box {
         }
         void print();
 };
+
 
 #endif
