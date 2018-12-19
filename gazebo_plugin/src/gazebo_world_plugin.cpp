1ec28c6c7898ce5caefd283db5f41cd294a52151
ErwanM <erwan.moreac@gmail.com>
2018-12-19 15:12:37 +0100

    Succeed to change Wind linear velocity in gazebo plugin

--- gazebo_plugin/src/gazebo_world_plugin.cpp
+++ gazebo_plugin/src/gazebo_world_plugin.cpp
@@ -12,57 +12,78 @@ namespace gazebo
 {
 class WorldPluginTutorial : public WorldPlugin
 {
-public:
-  WorldPluginTutorial() : WorldPlugin()
-  {
-  }
-
-  void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-  {
-    // Make sure the ROS node for Gazebo has already been initialized                                                                                    
-    if (!ros::isInitialized())
+  public:
+    WorldPluginTutorial() : WorldPlugin()
     {
-      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
-      return;
     }
 
-    ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
+    void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+    {
+        // Make sure the ROS node for Gazebo has already been initialized
+        if (!ros::isInitialized())
+        {
+            ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+                             << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+            return;
+        }
 
+        ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
 
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
 
-    ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
+        ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
 
-    // Option 3: Insert model from file via message passing.
-      // Create a new transport node
-      transport::NodePtr node(new transport::Node());
+        // Option 3: Insert model from file via message passing.
+        // Create a new transport node
+        transport::NodePtr node(new transport::Node());
 
-      // Initialize the node with the world name
-      node->Init(_world->Name());
+        // Initialize the node with the world name
+        node->Init(_world->Name());
 
-      // Create a publisher on the ~/factory topic
-      transport::PublisherPtr factoryPub =
-      node->Advertise<msgs::Factory>("~/factory");
+        // Create a publisher on the ~/factory topic
+        transport::PublisherPtr factoryPub =
+            node->Advertise<msgs::Factory>("~/factory");
 
-      // Create the message
-      msgs::Factory msg;
+        // Create the message
+        msgs::Factory msg;
 
-      // Model file to load
-      msg.set_sdf_filename("model:///Gazebo/Cylinder");
-      //home/labsticc/Bureau/HW_in_the_Loop/GitHub/HPeC_ws/src/mybot_gazebo/models/Gazebo/
-    
-      // Pose to initialize the model to
-      msgs::Set(msg.mutable_pose(),
-          ignition::math::Pose3d(
-            ignition::math::Vector3d(1, -2, 0),
-            ignition::math::Quaterniond(0, 0, 0)));
+        // Model file to load
+        msg.set_sdf_filename("model:///Gazebo/Cylinder");
 
-      // Send the message
-      factoryPub->Publish(msg);
-    
-  }
+        // Pose to initialize the model to
+        msgs::Set(msg.mutable_pose(),
+                  ignition::math::Pose3d(
+                      ignition::math::Vector3d(2, 2, 0),
+                      ignition::math::Quaterniond(0, 0, 0)));
+
+        // Send the message
+        factoryPub->Publish(msg);
+
+        transport::PublisherPtr windPub =
+            node->Advertise<msgs::Wind>("~/wind");
+        msgs::Wind msg_wind;
+
+        msgs::Set(msg_wind.mutable_linear_velocity(),
+                ignition::math::Vector3d(0, 5, 10)
+        );
+        windPub->Publish(msg_wind);
+
+        // Create a publisher on the ~/light/modify
+        //transport::PublisherPtr lightPub =
+        //node->Advertise<msgs::Light>("~/light/modify");
+        //msgs::Light msg_light;
+        //msg_light.set_name("sun");
+        //msg_light.set_parent_name(_world->Name());
+        //msg_light.set_allocated_diffuse(msgs::Color * diffuse()));
+        /*msgs::Set(msg_light.mutable_diffuse(),
+                msgs::Color()
+            );
+        lightPub->Publish(msg_light);
+      */
+    }
+
+  private:
 
 };
-GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
-}
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
+} // namespace gazebo
