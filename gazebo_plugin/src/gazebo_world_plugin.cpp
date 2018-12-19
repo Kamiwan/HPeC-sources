91d29e3a880b91645d67b3941b61eefbc37d2d92
ErwanM <erwan.moreac@gmail.com>
2018-12-19 11:39:17 +0100

    Add during simulation a Cylinder model in gazebo world
    * create a simple cylinder model
    * add in the plugin the communication system with gazebo in order to advertise the model creation
    * (i) use sleep_function in plugin make sleep the whole simulation => pthread needed

--- gazebo_plugin/src/gazebo_world_plugin.cpp
+++ gazebo_plugin/src/gazebo_world_plugin.cpp
@@ -1,5 +1,12 @@
 #include <gazebo/common/Plugin.hh>
+#include <ignition/math/Pose3.hh>
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include "gazebo/gazebo.hh"
 #include <ros/ros.h>
+
+#include <chrono>
+#include <thread>
 
 namespace gazebo
 {
@@ -21,6 +28,39 @@ public:
     }
 
     ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
+
+
+    //std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    ROS_INFO("Hello World FROM GAZEBO WORLD PLUGIN!");
+
+    // Option 3: Insert model from file via message passing.
+      // Create a new transport node
+      transport::NodePtr node(new transport::Node());
+
+      // Initialize the node with the world name
+      node->Init(_world->Name());
+
+      // Create a publisher on the ~/factory topic
+      transport::PublisherPtr factoryPub =
+      node->Advertise<msgs::Factory>("~/factory");
+
+      // Create the message
+      msgs::Factory msg;
+
+      // Model file to load
+      msg.set_sdf_filename("model:///Gazebo/Cylinder");
+      //home/labsticc/Bureau/HW_in_the_Loop/GitHub/HPeC_ws/src/mybot_gazebo/models/Gazebo/
+    
+      // Pose to initialize the model to
+      msgs::Set(msg.mutable_pose(),
+          ignition::math::Pose3d(
+            ignition::math::Vector3d(1, -2, 0),
+            ignition::math::Quaterniond(0, 0, 0)));
+
+      // Send the message
+      factoryPub->Publish(msg);
+    
   }
 
 };
