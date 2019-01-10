f12d53ac5a67a30f63932ffe5b378ab53496e4e1
ErwanM <erwan.moreac@gmail.com>
2019-01-10 13:56:03 +0100

    Add header comment in some files

--- gazebo_light_sensor_plugin/include/gazebo_light_sensor_plugin/light_sensor_plugin.h
+++ gazebo_light_sensor_plugin/include/gazebo_light_sensor_plugin/light_sensor_plugin.h
@@ -1,3 +1,29 @@
+/* 
+ * This file is part of the HPeC distribution (https://github.com/Kamiwan/HPeC-sources).
+ * Copyright (c) 2019 Lab-STICC Laboratory.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+/*************************************************************************************
+ * Author(s) :  Erwan Moréac, erwan.moreac@univ-ubs.fr (EM)
+ * Created on:  January 8, 2019
+ * 
+ * Main header file of light sensor plugin.
+ * Create a light sensor class which inherits of Camera sensor class to compute illuminance
+ *    
+ * Plugin derived from this tutorial: 
+ * https://github.com/SMARTlab-Purdue/ros-tutorial-gazebo-simulation/wiki/Sec.-4:-Creating-a-light-sensor-plugin
+ *************************************************************************************/
     #ifndef GAZEBO_ROS_LIGHT_SENSOR_HH
     #define GAZEBO_ROS_LIGHT_SENSOR_HH
     
--- gazebo_plugin/src/gazebo_world_plugin.cpp
+++ gazebo_plugin/src/gazebo_world_plugin.cpp
@@ -1,3 +1,32 @@
+/* 
+ * This file is part of the HPeC distribution (https://github.com/Kamiwan/HPeC-sources).
+ * Copyright (c) 2019 Lab-STICC Laboratory.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+/*************************************************************************************
+ * Author(s) :  Erwan Moréac, erwan.moreac@univ-ubs.fr (EM)
+ * Created on:  January 10, 2019
+ * 
+ * Main file of gazebo world plugin.
+ *  This program allow to create various scenario by generating dynamic events in the
+ *  simulator.
+ *  For instance, it is possible to change on the fly: 
+ *      - simulation parameters such as wind, gravity and so on...
+ *      - call a new model inside the world
+ *      - trigger UAV failures thanks to python mav_proxy scripts
+ *************************************************************************************/
+
 #include <gazebo/common/Plugin.hh>
 #include <ignition/math/Pose3.hh>
 #include "gazebo/physics/physics.hh"
